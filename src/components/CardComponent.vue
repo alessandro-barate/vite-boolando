fa34bcfba2e7faf46b6923df140e1ac0bbb1411f
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2024-05-30 18:16:59 +0200

    [UPD] Main cards imported

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "CardComponent",
+};
+</script>
+
+<template>
+  <div class="col-33-img">
+    <div class="content">
+      <img class="object" src="../../img/1.webp" alt="" />
+      <img class="object-hidden" src="../../img/1b.webp" alt="" />
+      <button class="heart bg-white top-position right-position">
+        &hearts;
+      </button>
+    </div>
+    <span class="positioned-boxes bg-red boldness">-50%</span>
+    <span class="positioned-boxes bg-green left-position boldness"
+      >Sostenibilità</span
+    >
+    <div>Levi's</div>
+    <div class="item-name boldness">RELAXED FIT TEE UNISEX</div>
+    <span class="red-text boldness">14,99&euro;</span>
+    <span class="text-line">29,99&euro;</span>
+  </div>
+</template>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,9 @@
 <script>
+import CardComponent from "./CardComponent.vue";
+
 export default {
   name: "MainComponent",
+  components: { CardComponent },
 };
 </script>
 
@@ -8,111 +11,29 @@ export default {
   <main>
     <div class="container">
       <div class="margin-top d-flex wrap">
-        <!-- FIRST ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/1.webp" alt="" />
-            <img class="object-hidden" src="../../img/1b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
-          <span class="positioned-boxes bg-red boldness">-50%</span>
-          <span class="positioned-boxes bg-green left-position boldness"
-            >Sostenibilità</span
-          >
-          <div>Levi's</div>
-          <div class="item-name boldness">RELAXED FIT TEE UNISEX</div>
-          <span class="red-text boldness">14,99&euro;</span>
-          <span class="text-line">29,99&euro;</span>
-        </div>
-        <!-- /FIRST ARTICLE -->
+        <!-- First article -->
+        <CardComponent />
+        <!--  FINE First article -->
 
-        <!-- SECOND ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/2.webp" alt="" />
-            <img class="object-hidden" src="../../img/2b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
-          <span class="positioned-boxes bg-red boldness">-30%</span>
-          <div>Guess</div>
-          <div class="item-name boldness">ROSES TEE</div>
-          <span class="red-text boldness">20,99&euro;</span>
-          <span class="text-line">29,99&euro;</span>
-        </div>
-        <!-- /SECOND ARTICLE -->
+        <!-- Second article -->
+        <CardComponent />
+        <!-- FINE Second article -->
 
-        <!-- THIRD ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/3.webp" alt="" />
-            <img class="object-hidden" src="../../img/3b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
-          <span class="positioned-boxes bg-red boldness">-30%</span>
-          <div>Come Zucchero Filato</div>
-          <div class="item-name boldness">VOGLIA DI COLORI PASTELLO</div>
-          <span class="red-text boldness">129,99&euro;</span>
-          <span class="text-line">184,99&euro;</span>
-        </div>
-        <!-- /THIRD ARTICLE -->
+        <!-- Third article -->
+        <CardComponent />
+        <!-- FINE Third article -->
 
-        <!-- FOURTH ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/4.webp" alt="" />
-            <img class="object-hidden" src="../../img/4b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
-          <span class="positioned-boxes bg-red boldness">-50%</span>
-          <span class="positioned-boxes bg-green left-position boldness"
-            >Sostenibilità</span
-          >
-          <div>Levi's</div>
-          <div class="item-name boldness">TEE UNISEX</div>
-          <span class="red-text boldness">14,99&euro;</span>
-          <span class="text-line">29,99&euro;</span>
-        </div>
-        <!-- /FOURTH ARTICLE -->
+        <!-- Fourth article -->
+        <CardComponent />
+        <!-- FINE Fourth article -->
 
-        <!-- FIFTH ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/5.webp" alt="" />
-            <img class="object-hidden" src="../../img/5b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
+        <!-- Fifth article -->
+        <CardComponent />
+        <!-- FINE Fifth article -->
 
-          <div>Maya Deluxe</div>
-          <div class="item-name boldness">STRIPE BODICE</div>
-          <span class="red-text boldness">99,99&euro;</span>
-        </div>
-        <!-- /FIFTH ARTICLE -->
-
-        <!-- SIXTH ARTICLE -->
-        <div class="col-33-img">
-          <div class="content">
-            <img class="object" src="../../img/6.webp" alt="" />
-            <img class="object-hidden" src="../../img/6b.webp" alt="" />
-            <button class="heart bg-white top-position right-position">
-              &hearts;
-            </button>
-          </div>
-          <span class="positioned-boxes bg-green boldness">Sostenibilità</span>
-          <div>Esprit</div>
-          <div class="item-name boldness">MAGLIONE - BLACK</div>
-          <span class="red-text boldness">29,99&euro;</span>
-        </div>
-        <!-- /SIXTH ARTICLE -->
+        <!-- Sixth article -->
+        <CardComponent />
+        <!-- FINE Sixth article -->
       </div>
     </div>
   </main>
