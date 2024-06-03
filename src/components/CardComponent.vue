21bb426e7a30223aa5103e5744f8e062f13e7792
alessandro-barate <alebarat@gmail.com>
2024-06-03 09:03:59 +0100

    [UPD] Back images

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -7,7 +7,7 @@ export default {
         {
           id: 1,
           frontImage: "../img/1.webp",
-          backImage: "1b.webp",
+          backImage: "../img/1b.webp",
           brand: "Levi's",
           name: "Relaxed Fit",
           price: 29.99,
@@ -26,7 +26,7 @@ export default {
         {
           id: 2,
           frontImage: "../img/2.webp",
-          backImage: "2b.webp",
+          backImage: "../img/2b.webp",
           brand: "Guess",
           name: "Roses Tee",
           price: 20.99,
@@ -41,7 +41,7 @@ export default {
         {
           id: 3,
           frontImage: "../img/3.webp",
-          backImage: "3b.webp",
+          backImage: "../img/3b.webp",
           brand: "Come Zucchero Filato",
           name: "Voglia di colori pastello",
           price: 129.99,
@@ -56,7 +56,7 @@ export default {
         {
           id: 4,
           frontImage: "../img/4.webp",
-          backImage: "4b.webp",
+          backImage: "../img/4b.webp",
           brand: "Levi's",
           name: "Tee Unisex",
           price: 14.99,
@@ -75,7 +75,7 @@ export default {
         {
           id: 5,
           frontImage: "../img/5.webp",
-          backImage: "5b.webp",
+          backImage: "../img/5b.webp",
           brand: "Maya Deluxe",
           name: "Stripe Bodice",
           price: 99.99,
@@ -94,7 +94,7 @@ export default {
         {
           id: 6,
           frontImage: "../img/6.webp",
-          backImage: "6b.webp",
+          backImage: "../img/6b.webp",
           brand: "Esprit",
           name: "Maglione - Black",
           price: 29.99,
